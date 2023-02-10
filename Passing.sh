985fc5ef85041c6d90886ec40ab98d872d68b39e
Igor Telinge <igor.telinge75@gmail.com>
2023-02-10 18:42:10 +0100

    3eme exo

---
+++ Passing.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+function File {
+    # think you are inside the file
+    # Change Here
+    echo $#
+}
+
+# Do not change anything
+if [ ! $# -lt 1 ]; then
+    File $*
+    exit 0
+fi
+
+# change here
+# here you can pass the arguments
+bash Passing.sh Shell is fun
